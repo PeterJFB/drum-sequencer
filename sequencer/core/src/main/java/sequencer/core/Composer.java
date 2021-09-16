0ff1a63e742e8dfa753d827303c9111c9db7af52
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-09-16 13:59:01 +0200

    Created mvp composer with hard coded track

--- sequencer/core/src/main/java/sequencer/core/Composer.java
+++ sequencer/core/src/main/java/sequencer/core/Composer.java
@@ -3,19 +3,25 @@ package sequencer.core;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import javafx.scene.media.AudioClip;
+
 public class Composer {
     public static final int BPM = 128; //Beats per minute. One beat is one fourth of a measure, or four sixtheenths //TODO: Get the BPM from the track instead of having a set BPM
     public static final int MEASURE_LENGTH = 16; //How long the measure is (In sixteenths) //TODO: Get the measure Length from the track instead of having a set length
 
-    private int beatProgress; //How many sixteenths of the measure has been played
+    private static final AudioClip kick = new AudioClip(Composer.class.getResource("/sequencer/core/707 Kick.wav").toExternalForm());
+    private static final AudioClip hihat = new AudioClip(Composer.class.getResource("/sequencer/core/808 CH.wav").toExternalForm());
+    private static final AudioClip snare = new AudioClip(Composer.class.getResource("/sequencer/core/808 Snare.wav").toExternalForm());
+
+    private int progress; //How many sixteenths of the measure has been played
     private Timer timer;
 
     Composer(){
-        beatProgress = 0;
+        progress = 0;
         timer = new Timer();
     }
 
-    private void start() {
+    public void start() {
         //Sets up a scheduled timer task to fire progressBeat(), where the time between sixteenths is calculated in millisecondsBetweenSixteenths()
         timer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
@@ -32,12 +38,19 @@ public class Composer {
     }
 
     private void progressBeat(){
-        System.out.println(beatProgress);
-        beatProgress ++;
-        beatProgress = beatProgress % MEASURE_LENGTH;
+        //Runs every sixteenths. Plays everything that is set for the current
+        if (progress % 4 == 0) {
+            kick.play();
+        }
+        if (progress % 8 == 0 || progress == 11) {
+            snare.play();
+        }
+        hihat.play();
+        progress ++;
+        progress = progress % MEASURE_LENGTH;
     }
 
-    private void stop() {
+    public void stop() {
         timer.cancel();
     }
 

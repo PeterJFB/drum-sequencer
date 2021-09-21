44171b4e3de54702ebff2ab7674d6e4b3b77e61f
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-09-21 08:51:35 +0200

    Plays from current Track

--- sequencer/core/src/main/java/sequencer/core/Composer.java
+++ sequencer/core/src/main/java/sequencer/core/Composer.java
@@ -1,5 +1,8 @@
 package sequencer.core;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -9,13 +12,20 @@ public class Composer {
     public static final int BPM = 128; //Beats per minute. One beat is one fourth of a measure, or four sixtheenths //TODO: Get the BPM from the track instead of having a set BPM
     public static final int MEASURE_LENGTH = 16; //How long the measure is (In sixteenths) //TODO: Get the measure Length from the track instead of having a set length
 
-    private static final AudioClip kick = new AudioClip(Composer.class.getResource("/sequencer/core/707 Kick.wav").toExternalForm());
-    private static final AudioClip hihat = new AudioClip(Composer.class.getResource("/sequencer/core/808 CH.wav").toExternalForm());
-    private static final AudioClip snare = new AudioClip(Composer.class.getResource("/sequencer/core/808 Snare.wav").toExternalForm());
+    private static final Map<String, AudioClip> instrumentAudioClips;
+    static {
+        Map<String, AudioClip> instrMap = new HashMap<>();
+        instrMap.put("kick", new AudioClip(Composer.class.getResource("/sequencer/core/707 Kick.wav").toExternalForm()));
+        instrMap.put("hihat", new AudioClip(Composer.class.getResource("/sequencer/core/808 CH.wav").toExternalForm()));
+        instrMap.put("snare", new AudioClip(Composer.class.getResource("/sequencer/core/808 Snare.wav").toExternalForm()));
+        instrumentAudioClips = Collections.unmodifiableMap(instrMap);
+    }
 
     private int progress; //How many sixteenths of the measure has been played
     private Timer timer;
     private boolean playing;
+
+    private Track currentTrack;
 
     /** 
      * Composer constructor. Equal to Composer(true).
@@ -35,6 +45,14 @@ public class Composer {
         progress = 0;
         timer = new Timer(createDaemonTimer);
         playing = false;
+    }
+
+    /**
+     * Choose which track to play
+     * @param track The track to play
+     */
+    public void chooseTrack(Track track){
+        currentTrack = track;
     }
 
     /** 
@@ -80,23 +98,23 @@ public class Composer {
     }
 
     /** 
-     * Runs every sixteenth. Plays everything that is set for the current
+     * Runs every sixteenth. Plays everything that is set for the current sixteenth
      */
     private void progressBeat(){
-        //TODO: Read what instruments to play from a track instead of hardcoded
-        if (progress % 4 == 0) {
-            kick.play();
-        }
-        if (progress % 8 == 0 || progress == 11) {
-            snare.play();
-        }
-        hihat.play();
+        currentTrack.getInstruments().keySet().stream()
+        .filter(instrument -> {
+            return currentTrack.getInstruments().get(instrument).get(progress);
+        })
+        .forEach(instrument -> {
+            instrumentAudioClips.get(instrument).play();
+        });
+
         progress ++;
         progress = progress % MEASURE_LENGTH;
     }
     
     /** 
-     * @return int whitch sixteenth the Conductor will play next
+     * @return int which sixteenth the Conductor will play next
      */
     public int getProgress() {
         return progress;
@@ -108,6 +126,20 @@ public class Composer {
      */
     public static void main(String[] args) {
         Composer composer = new Composer(false);
+
+        Track testTrack = new Track();
+        testTrack.addInstrument("kick", 16);
+        testTrack.addInstrument("snare", 16);
+        testTrack.updateInstrument("kick", 0);
+        testTrack.updateInstrument("kick", 4);
+        testTrack.updateInstrument("snare", 4);
+        testTrack.updateInstrument("kick", 8);
+        testTrack.updateInstrument("kick", 12);
+        testTrack.updateInstrument("snare", 12);
+        testTrack.updateInstrument("snare", 15);
+
+        composer.chooseTrack(testTrack);
+
         composer.start();
     }
 }

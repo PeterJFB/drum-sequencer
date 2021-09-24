5b22334b6063ff0713a8387df8c0a4c1c7abd4eb
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-09-24 15:29:29 +0200

    Get BPM and length from Track

--- sequencer/core/src/main/java/sequencer/core/Conductor.java
+++ sequencer/core/src/main/java/sequencer/core/Conductor.java
@@ -11,8 +11,6 @@ import java.util.TimerTask;
 import javafx.scene.media.AudioClip;
 
 public class Conductor {
-    public static final int BPM = 128; //Beats per minute. One beat is one fourth of a measure, or four sixtheenths //TODO: Get the BPM from the track instead of having a set BPM
-    public static final int MEASURE_LENGTH = 16; //How long the measure is (In sixteenths) //TODO: Get the measure Length from the track instead of having a set length
 
     private static final Map<String, AudioClip> instrumentAudioClips;
     static {
@@ -101,7 +99,7 @@ public class Conductor {
         timer.scheduleAtFixedRate(
         progressBeatTask,
         0,
-        millisecondsBetweenSixteenths(BPM)); 
+        millisecondsBetweenSixteenths(Track.BPM)); 
         playing = true;
     }
 
@@ -141,7 +139,7 @@ public class Conductor {
         listeners.forEach(listener -> listener.run(progress));
 
         progress ++;
-        progress = progress % MEASURE_LENGTH;
+        progress = progress % Track.TRACK_LENGTH;
     }
     
     /** 

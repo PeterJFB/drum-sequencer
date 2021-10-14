ac23484f44f117708d85cdeb1bb0362febf0a3ea
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-10-14 10:59:35 +0200

    Fixed checkstyle

--- sequencer/core/src/test/java/sequencer/core/ComposerTest.java
+++ sequencer/core/src/test/java/sequencer/core/ComposerTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.util.List;
 
 import java.util.stream.IntStream;

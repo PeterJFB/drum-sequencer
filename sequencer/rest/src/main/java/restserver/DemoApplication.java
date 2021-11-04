679652d6ce14fda5da2668d40a49be00add7bdc2
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-11-04 09:45:24 +0100

    Related to #74. Add dependency to PersistenceHandler

--- sequencer/rest/src/main/java/restserver/DemoApplication.java
+++ sequencer/rest/src/main/java/restserver/DemoApplication.java
@@ -1,14 +1,16 @@
-package rest.restserver;
+package restserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import sequencer.persistence.PersistenceHandler;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+  private PersistenceHandler persistenceHandler;
 
   public static void main(String[] args) {
     SpringApplication.run(DemoApplication.class, args);
@@ -17,5 +19,11 @@ public class DemoApplication {
   @GetMapping("/hello")
   public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
     return String.format("Hello %s!", name);
+  }
+
+  @GetMapping("/tracks")
+  public String tracks() {
+    persistenceHandler = new PersistenceHandler("drum-sequencer-persistence", "json");
+    return String.join("<br />", persistenceHandler.listFilenames());
   }
 }

fc20dd3b66484aa2b0239b10890ab4b466606e95
Juni Weisteen Bjerde <juni.w.bjerde@gmail.com>
2021-11-05 17:28:00 +0100

    Related to #64. Remove redundant imports

--- sequencer/fxui/src/main/java/sequencer/ui/SequencerController.java
+++ sequencer/fxui/src/main/java/sequencer/ui/SequencerController.java
@@ -1,9 +1,7 @@
 package sequencer.ui;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.io.UncheckedIOException;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;

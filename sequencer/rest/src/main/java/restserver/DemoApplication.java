cc19bfb1e1be402eb17f7171948e87afa85cfe46
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-11-05 15:58:19 +0100

    Related to #74. POST track

--- sequencer/rest/src/main/java/restserver/DemoApplication.java
+++ sequencer/rest/src/main/java/restserver/DemoApplication.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sequencer.persistence.PersistenceHandler;
@@ -26,7 +28,7 @@ public class DemoApplication {
    * Returns a collection of all tracks.
    */
   @GetMapping(value = "/tracks", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Collection<String> tracks() {
+  public Collection<String> getTracks() {
     persistenceHandler = new PersistenceHandler("drum-sequencer-persistence", "json");
     return persistenceHandler.listFilenames();
   }
@@ -38,11 +40,11 @@ public class DemoApplication {
    * @return the track, or the text "Track not found" with an error code of 404
    */
   @GetMapping(value = "/track", produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<String> track(@RequestParam(value = "name") String name) {
+  public ResponseEntity<String> getTrack(@RequestParam(value = "name") String name) {
     persistenceHandler = new PersistenceHandler("drum-sequencer-persistence", "json");
     StringBuilder stringBuilder = new StringBuilder();
     try {
-      persistenceHandler.readFromFile(name, (reader) -> {
+      persistenceHandler.readFromFile(name, reader -> {
         BufferedReader bufferedReader = new BufferedReader(reader);
         String line;
         try {
@@ -57,5 +59,32 @@ public class DemoApplication {
       return new ResponseEntity<>("Track not found", HttpStatus.NOT_FOUND);
     }
     return new ResponseEntity<>(stringBuilder.toString(), HttpStatus.OK);
+  }
+
+  /**
+   * Save a track to a file.
+   *
+   * @param trackAsJson The track as a JSON-object
+   * @param name the name of the track to save
+   * @return "fail" or "success" with error codes
+   */
+  @PostMapping("/track")
+  public ResponseEntity<String> postTrack(@RequestBody String trackAsJson,
+      @RequestParam(value = "name") String name) {
+    persistenceHandler = new PersistenceHandler("drum-sequencer-persistence", "json");
+    try {
+      persistenceHandler.writeToFile(name, writer -> {
+        try {
+          writer.write(trackAsJSON);
+        } catch (IOException e) {
+          System.err.println(e.getMessage());
+        }
+
+      });
+    } catch (IOException e) {
+      return new ResponseEntity<>("fail", HttpStatus.I_AM_A_TEAPOT);
+    }
+
+    return new ResponseEntity<>("success", HttpStatus.OK);
   }
 }

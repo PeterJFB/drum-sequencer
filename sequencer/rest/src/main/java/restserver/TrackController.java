9acfe97263de3a5fa903552fd67090ca10beefe9
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-11-07 19:28:04 +0100

    Related to #74. Split REST Application and Controller

--- sequencer/rest/src/main/java/restserver/TrackController.java
+++ sequencer/rest/src/main/java/restserver/TrackController.java
@@ -3,8 +3,6 @@ package restserver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Collection;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sequencer.persistence.PersistenceHandler;
 
-@SpringBootApplication
 @RestController
-public class DemoApplication {
+public class TrackController {
   private PersistenceHandler persistenceHandler;
-
-  public static void main(String[] args) {
-    SpringApplication.run(DemoApplication.class, args);
-  }
 
   /**
    * Returns a collection of all tracks.
---
+++ sequencer/rest/src/main/java/restserver/TrackStoreApplication.java
@@ -0,0 +1,13 @@
+package restserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TrackStoreApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(TrackStoreApplication.class, args);
+  }
+}

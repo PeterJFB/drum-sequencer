277ae8a2c33a1ad8b8e9df22f7e69d060b273dfb
PeterJFB <peterj.flbj@gmail.com>
2021-11-24 12:53:20 +0100

    Related to #93. Review Track class

--- sequencer/core/src/main/java/sequencer/core/Track.java
+++ sequencer/core/src/main/java/sequencer/core/Track.java
@@ -6,13 +6,16 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * The {@link Track} class represents a collection of instruments programmed to a specific beat, and
- * is also used to edit the progression of these instruments.
+ * The {@link Track} class represents a group of instruments programmed to a specific pattern, and
+ * is also used to edit the pattern of these instruments.
  */
 public class Track {
 
-  public static final int TRACK_LENGTH = 16;
   public static final int BPM = 128;
+  public static final int TRACK_LENGTH = 16;
+
+  public static final int TRACK_NAME_LENGTH = 30;
+  public static final int ARTIST_NAME_LENGTH = 30;
 
   private String trackName;
   private String artistName;
@@ -20,63 +23,66 @@ public class Track {
 
   /**
    * Constructor for creating an empty track. We always construct the track empty and then add
-   * instruments and trackName and artistName later on
+   * instruments, trackName and artistName later.
    */
   public Track() {}
 
   /**
-   * Get name of track.
+   * Get the name of the track.
    *
-   * @return trackName, or null if trackName is not set yet
+   * @return trackName, or null if trackName is not set
    */
   public String getTrackName() {
     return trackName;
   }
 
   /**
-   * Get name of who made track.
+   * Get the name of who made the track.
    *
-   * @return artistName, or null if artistName is not set yet
+   * @return artistName, or null if artistName is not set
    */
   public String getArtistName() {
     return artistName;
   }
 
   /**
-   * Get all added instruments by their name (key).
+   * Get all added instruments by their name.
    *
-   * @return a List of instrument names as Strings. The reason this is a List and not a Collection
-   *         is so that when saving tracks, the order in which you added the instruments is kept in
-   *         place, which makes for a better user experience when saving and loading tracks.
+   * @return a List of instrument names as {@link String}s. The reason this is a List and not a
+   *         Collection is so that when saving tracks, the order in which you added the instruments
+   *         is kept in place, which makes for a better user experience when saving and loading
+   *         tracks.
    */
   public List<String> getInstrumentNames() {
     return new ArrayList<>(instruments.keySet());
   }
 
   /**
-   * Get pattern of an added instrument.
+   * Get the pattern of an instrument in the track.
    *
-   * @param instrument the instrument which we want the pattern from
-   * @return a copy of the pattern for the given instrument or null instrument is not a key in
-   *         instruments
+   * @param instrumentName the instrument which we want the pattern from
+   * @return a copy of the pattern for the given instrument, or null if the instrument is not in the
+   *         track
    */
-  public List<Boolean> getPattern(String instrument) {
-    if (instrument == null || !instruments.containsKey(instrument)) {
+  public List<Boolean> getPattern(String instrumentName) {
+    if (instrumentName == null || !instruments.containsKey(instrumentName)) {
       throw new IllegalArgumentException(
           "Could not find pattern for instrument since it was not part of the track");
     }
-    return new ArrayList<>(instruments.get(instrument));
+    return new ArrayList<>(instruments.get(instrumentName));
   }
 
   /**
-   * Changes name of track.
+   * Changes the name of the track.
    *
    * @param trackName new name of track
-   * @throws IllegalArgumentException if trackName is more than 30 characters long
+   * @throws IllegalArgumentException if trackName is more than TRACK_NAME_LENGTH characters long.
    */
   public void setTrackName(String trackName) throws IllegalArgumentException {
-    if (trackName != null && trackName.length() >= 30) {
-      throw new IllegalArgumentException("Track name can not be more than 30 characters long.");
+    if (trackName != null && trackName.length() > TRACK_NAME_LENGTH) {
+      throw new IllegalArgumentException(
+          "Track name cannot be more than %s characters long. Found %s".formatted(TRACK_NAME_LENGTH,
+              trackName.length()));
     }
     this.trackName = trackName;
   }
@@ -85,17 +91,19 @@ public class Track {
    * Changes name of who made the track.
    *
    * @param artistName new name of artist
-   * @throws IllegalArgumentException if artistName is more than 30 characters long
+   * @throws IllegalArgumentException if artistName is more than ARTIST_NAME_LENGTH characters long
    */
   public void setArtistName(String artistName) throws IllegalArgumentException {
-    if (artistName != null && artistName.length() >= 30) {
-      throw new IllegalArgumentException("Artist name can not be more than 30 characters long.");
+    if (artistName != null && artistName.length() > ARTIST_NAME_LENGTH) {
+      throw new IllegalArgumentException(
+          "Artist name cannot be more than %s characters long. Found %s"
+              .formatted(ARTIST_NAME_LENGTH, artistName.length()));
     }
     this.artistName = artistName;
   }
 
   /**
-   * Adds another instrument to instruments with given values.
+   * Adds another instrument to the track with the given pattern.
    *
    * @param instrument name of the new instrument
    * @param pattern the pattern to apply with the instrument. The reason this is a List and not a
@@ -114,59 +122,61 @@ public class Track {
   }
 
   /**
-   * Adds another instrument to instruments with given length an all values as false.
+   * Adds another instrument to the track with length TRACK_LENGTH and sets all values/sixteenths in
+   * the pattern to be inactive (false).
    *
-   * @param instrument name of the new instrument
+   * @param instrumentName name of the new instrument
    */
-  public void addInstrument(String instrument) {
-    List<Boolean> pattern = new ArrayList<>();
+  public void addInstrument(String instrumentName) {
+    final List<Boolean> pattern = new ArrayList<>();
     for (int i = 0; i < TRACK_LENGTH; i++) {
       pattern.add(false);
     }
-    addInstrument(instrument, pattern);
+    addInstrument(instrumentName, pattern);
   }
 
   /**
-   * removes an instrument from instruments.
+   * removes an instrument from the track.
    *
-   * @param instrument name of the instrument to be removed
+   * @param instrumentName name of the instrument to be removed
    * @throws IllegalArgumentException if the instrument to be removed is not part of the track
    */
-  public void removeInstrument(String instrument) {
-    if (instrument == null || !instruments.containsKey(instrument)) {
+  public void removeInstrument(String instrumentName) {
+    if (instrumentName == null || !instruments.containsKey(instrumentName)) {
       throw new IllegalArgumentException(
           "Instrument could not be removed since it was not part of the track");
     }
-    instruments.remove(instrument);
+    instruments.remove(instrumentName);
   }
 
   /**
-   * Toggles the value of the given sixteenth for the pattern of the given instrument (from true to
-   * false, or vise versa).
+   * Toggles the value/sixteenth in the pattern of the given instrument (from true to false, or vise
+   * versa).
    *
-   * @param instrument name of the instrument which is to be toggled
-   * @param sixteenth index in the instruments pattern
+   * @param instrumentName name of the instrument which is to be toggled
+   * @param sixteenthIndex index of the sixteenth in the instruments pattern
    * @throws IllegalArgumentException if instrument is not a key in instruments, or sixteenth (the
    *         index) is out of bounds
    */
-  public void toggleSixteenth(String instrument, int sixteenth) throws IllegalArgumentException {
-    List<Boolean> pattern = instruments.get(instrument);
+  public void toggleSixteenth(String instrumentName, int sixteenthIndex)
+      throws IllegalArgumentException {
+    final List<Boolean> pattern = instruments.get(instrumentName);
     if (pattern == null) {
       throw new IllegalArgumentException("Cannot update non-existing instrument");
     }
-    if (0 > sixteenth || sixteenth >= TRACK_LENGTH) {
+    if (0 > sixteenthIndex || sixteenthIndex >= TRACK_LENGTH) {
       throw new IllegalArgumentException(
-          "Cannot update instrument. Sixteenth index is outside track length (%s): %s"
-              .formatted(TRACK_LENGTH, sixteenth));
+          "Cannot update instrument. Sixteenth index is outside the track bounds [0-%s): %s"
+              .formatted(TRACK_LENGTH, sixteenthIndex));
     }
-    pattern.set(sixteenth, !pattern.get(sixteenth));
+    pattern.set(sixteenthIndex, !pattern.get(sixteenthIndex));
   }
 
   /**
    * Returns a copy of this track.
    */
   public Track copy() {
-    Track newTrack = new Track();
+    final Track newTrack = new Track();
     newTrack.setArtistName(artistName);
     newTrack.setTrackName(trackName);
     for (String instrument : getInstrumentNames()) {
